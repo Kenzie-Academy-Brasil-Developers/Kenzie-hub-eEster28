80cdb313ca3978bf171f508334ae570663451fe1
Ester Moura <135028149+eEster28@users.noreply.github.com>
2023-10-23 17:19:51 -0300

    feat: adicionando proteção de rota

--- src/routes/RoutesMain.jsx
+++ src/routes/RoutesMain.jsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom"
 import { LoginPage } from "../pages/LoginPage"
 import { RegisterPage } from "../pages/RegisterPage"
 import { DeshboardPage } from "../pages/DeshboardPage"
-import { useState } from "react"
+import { PrivateRoutes } from "./PrivateRoutes"
 
 export const RoutesMain = () => {
     return (
         <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/deshboard" element={<DeshboardPage/>} />
+            <Route element={<PrivateRoutes />}>
+                <Route path="/deshboard" element={<DeshboardPage />} />
+            </Route>
         </Routes>
     )
 }
